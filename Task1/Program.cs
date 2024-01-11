c35dc74afc2561bf293b61ad95ddd013602d6d11
“iamIlyaAlex” <blagas77@me.com>
2024-01-11 17:28:32 +0300

    added less2/Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int number = 1234567;
+int lastDigit = (number / 10 / 10 / 10);
+Console.WriteLine(lastDigit);
