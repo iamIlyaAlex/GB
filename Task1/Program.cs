e4b5b79db2dd02e779c54f057871a7811b86dfff
“iamIlyaAlex” <blagas77@me.com>
2024-01-12 12:45:29 +0300

    added information to/Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 // Console.WriteLine("Hello, World!");
 
-int number = 1234567;
-int lastDigit = (number / 10 / 10 / 10);
-Console.WriteLine(lastDigit);
+internal class Program
+{
+    static void Main()
+    {
+        // int number = 9542; // % остаток от деления - вывод последней цифры
+        // int lastDigit = number % 10; // / деление - удаление последней цифры
+        // // Console.WriteLine(lastDigit);
+
+        // Console.WriteLine(lastDigit); // shift + alt + f (наводит порядок в тексте)
+
+
+        // number = number / 10; // num =>954
+        // lastDigit = number % 10; 
+        // Console.WriteLine(lastDigit);
+
+        // number = number / 10; // num =>95
+        // lastDigit = number % 10; 
+        // Console.WriteLine(lastDigit);
+
+        // number = number / 10; // num =>9
+        // lastDigit = number % 10; 
+        // Console.WriteLine(lastDigit);
+
+
+        long number = 5000000000;
+        int lastDigit;
+
+        while (?????)
+
+        lastDigit = number % 10;
+        Console.WriteLine(lastDigit);
+        number = number / 10; // num =>954
+
+        lastDigit = number % 10;
+        Console.WriteLine(lastDigit);
+        number = number / 10; // num =>954
+
+        lastDigit = number % 10;
+        Console.WriteLine(lastDigit);
+        number = number / 10; // num =>954
+
+        lastDigit = number % 10;
+        Console.WriteLine(lastDigit);
+        number = number / 10; // num =>954
+        
+
+
+    }
+}
